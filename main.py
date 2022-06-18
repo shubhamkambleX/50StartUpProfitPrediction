224990f73c058f409ae43a5a6d6ecdd4ccb5630d
shubham kamble <196shubham@gmail.com>
2022-06-18 20:38:49 +0530

    files addedd for profit prediction

---
+++ main.py
@@ -0,0 +1,59 @@
+import json
+from wsgiref import simple_server
+from flask import Flask, request, render_template
+import pickle
+import numpy as np
+
+app=Flask(__name__)
+
+def get_predict_profit(r_d_expenses,administration_expenses,marketing_expenses,state):
+
+    with open('models/profit_predict_model.pkl','rb') as f:
+        model = pickle.load(f)
+
+    with open('models/columns.json',"r") as f:
+        data_columns = json.load(f)['data_columns']
+        print(data_columns)
+    try:
+        state_index = data_columns.index('state_'+str(state).lower())
+        print(state_index)
+    except:
+        state_index = -1
+
+    x = np.zeros(len(data_columns))
+    x[0] = r_d_expenses
+    x[1] = administration_expenses
+    x[2] = marketing_expenses
+
+    if state_index >= 0:
+        x[state_index] = 1
+
+
+    return round(model.predict([x])[0],2)
+
+
+
+
+@app.route("/")
+def index_page():
+    return render_template('index.html')
+
+@app.route("/predict",methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        r_d_expenses = request.form['r_d_expenses']
+        administration_expenses = request.form['administration_expenses']
+        marketing_expenses = request.form['marketing_expenses']
+        state = request.form['state']
+        output = get_predict_profit(r_d_expenses,administration_expenses,marketing_expenses,state)
+        return render_template('index.html',show_hidden=True,prediction_text='StartUp Profit must be $ {}'.format(output))
+
+
+
+
+if __name__=="__main__":
+    # app.run(debug=True)
+    host = '0.0.0.0'
+    port = 5000
+    httpd = simple_server.make_server(host,port,app)
+    httpd.serve_forever()
